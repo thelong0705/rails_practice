dd77cf36faa8c8609b5462b158aaa6b5b1b7d463
thelong0705 <thelong0705@gmail.com>
2019-10-17 10:35:26 +0700

    add delete function

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -27,6 +27,12 @@ class TasksController < ApplicationController
     redirect_to tasks_url, notice: "Updated #{task.name}"
   end
 
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy!
+    redirect_to tasks_url, notice: "Deleted #{task.name}"
+  end
+
   private
 
   def task_params
