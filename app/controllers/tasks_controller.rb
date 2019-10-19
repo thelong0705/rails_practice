9757a5ef0f0cb8868d66781f41efb3d46ebff9c8
thelong0705 <thelong0705@gmail.com>
2019-10-19 10:06:11 +0700

    add before action method for task controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[show edit update destroy]
+
   def index
     @tasks = current_user.tasks.recent
   end
 
   def show
-    @task = current_user.tasks.find(params[:id])
   end
 
   def new
@@ -12,7 +13,6 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = current_user.tasks.find(params[:id])
   end
 
   def create
@@ -26,20 +26,23 @@ class TasksController < ApplicationController
   end
 
   def update
-    task = current_user.tasks.find(params[:id])
-    task.update!(task_params)
-    redirect_to tasks_url, notice: "Updated #{task.name}"
+    @task.update!(task_params)
+    redirect_to tasks_url, notice: "Updated #{@task.name}"
   end
 
   def destroy
-    task = current_user.tasks.find(params[:id])
-    task.destroy!
-    redirect_to tasks_url, notice: "Deleted #{task.name}"
+    @task = current_user.tasks.find(params[:id])
+    @task.destroy!
+    redirect_to tasks_url, notice: "Deleted #{@task.name}"
   end
 
   private
 
   def task_params
     params.require(:task).permit(:name, :description)
+  end
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
   end
 end
