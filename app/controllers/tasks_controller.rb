d29aecbe420f0b9ba1f3a72e396e17719e8b6e3b
thelong0705 <thelong0705@gmail.com>
2019-10-19 09:45:42 +0700

    filter task by user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,10 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks
   end
 
   def show
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
   end
 
   def new
@@ -12,27 +12,27 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.new(task_params)
 
     if @task.save
-      redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。"
+      redirect_to @task, notice: "Created #{@task.name}"
     else
       render :new
     end
   end
 
   def update
-    task = Task.find(params[:id])
+    task = current_user.tasks.find(params[:id])
     task.update!(task_params)
     redirect_to tasks_url, notice: "Updated #{task.name}"
   end
 
   def destroy
-    task = Task.find(params[:id])
+    task = current_user.tasks.find(params[:id])
     task.destroy!
     redirect_to tasks_url, notice: "Deleted #{task.name}"
   end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,8 @@
 class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30}
-
   validate :validate_name_not_including_comma
+
+  belongs_to :user
 
   private
 
