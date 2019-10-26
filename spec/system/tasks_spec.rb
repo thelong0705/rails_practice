1124fe39390bcfe472bcd868fd1a3bc8e1457c19
The Long <thelong0705@gmail.com>
2019-10-26 19:28:41 +0700

    show task detail

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -3,17 +3,19 @@
 require 'rails_helper'
 
 describe 'CRUD tasks', type: :system do
-  describe 'show all tasks' do
-    let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
-    let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
+  let(:user_a) { FactoryBot.create(:user, name: 'user A', email: 'a@example.com') }
+  let(:user_b) { FactoryBot.create(:user, name: 'user B', email: 'b@example.com') }
+  let!(:task_a) { FactoryBot.create(:task, name: 'first task', user: user_a) }
 
-    before do
-      FactoryBot.create(:task, name: 'first task', user: user_a)
-      visit login_path
-      fill_in 'Email', with: login_user.email
-      fill_in 'Password', with: login_user.password
-      click_button 'login'
-    end
+  before do
+    # FactoryBot.create(:task, name: 'first task', user: user_a)
+    visit login_path
+    fill_in 'Email', with: login_user.email
+    fill_in 'Password', with: login_user.password
+    click_button 'login'
+  end
+
+  describe 'show all tasks' do
     context 'when user A login' do
       let(:login_user) { user_a }
 
@@ -26,6 +28,20 @@ describe 'CRUD tasks', type: :system do
       let(:login_user) { user_b }
       it 'dont show user A created task' do
         expect(page).to have_no_content 'first task'
+      end
+    end
+  end
+
+  describe 'task details' do
+    context 'when user A login' do
+      let(:login_user) { user_a }
+
+      before do
+        visit task_path(task_a)
+      end
+
+      it 'show task detail' do
+        expect(page).to have_content 'first task'
       end
     end
   end
