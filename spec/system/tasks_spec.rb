65dc980a69a0be6796b9238535185bcc959acd10
The Long <thelong0705@gmail.com>
2019-10-26 18:10:04 +0700

    refactor with let

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,18 +1,21 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 describe 'CRUD tasks', type: :system do
   describe 'show all tasks' do
+    let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
+    let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
+
     before do
-      user_a = FactoryBot.create(:user, name: 'user A', email: 'a@example.com')
       FactoryBot.create(:task, name: 'first task', user: user_a)
+      visit login_path
+      fill_in 'Email', with: login_user.email
+      fill_in 'Password', with: login_user.password
+      click_button 'login'
     end
     context 'when user A login' do
-      before do
-        visit login_path
-        fill_in 'Email', with: 'a@example.com'
-        fill_in 'Password', with: 'password'
-        click_button 'login'
-      end
+      let(:login_user) { user_a }
 
       it 'show all tasks that user A created' do
         expect(page).to have_content 'first task'
@@ -20,14 +23,7 @@ describe 'CRUD tasks', type: :system do
     end
 
     context 'when user B login' do
-      before do
-        FactoryBot.create(:user, name: 'user B', email: 'b@example.com')
-        visit login_path
-        fill_in 'Email', with: 'b@example.com'
-        fill_in 'Password', with: 'password'
-        click_button 'login'
-      end
-
+      let(:login_user) { user_b }
       it 'dont show user A created task' do
         expect(page).to have_no_content 'first task'
       end
